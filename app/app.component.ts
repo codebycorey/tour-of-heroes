3c769cb16eeea02c00c3ca1c97f5f362dfa229e9
rcodonnell <rcoreyodonnell@gmail.com>
2016-05-23 01:02:20 -0400

    added routing for heroes and a dashboard

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,39 @@
 import { Component }       from '@angular/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+
 import { HeroService }     from './hero.service';
 import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+
 @Component({
-  selector: 'my-app',
-  template: `
-    <h1>{{title}}</h1>
-    <my-heroes></my-heroes>
-  `,
-  directives: [HeroesComponent],
-  providers: [
-    HeroService
-  ]
+    selector: 'my-app',
+    template: `
+        <h1>{{title}}</h1>
+        <a [routerLink]="[Dashboard]">Dashboard</a>
+        <a [routerLink]="['Heroes']">Heroes</a>
+        <router-outlet></router-outlet>
+    `,
+    directives: [
+        ROUTER_DIRECTIVES
+    ],
+    providers: [
+        ROUTER_PROVIDERS,
+        HeroService,
+    ]
 })
+@RouteConfig([
+    {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: DashboardComponent,
+        useAsDefault: true
+    },
+    {
+        path: '/heroes',
+        name: 'Heroes',
+        component: HeroesComponent
+    }
+])
 export class AppComponent {
-  title = 'Tour of Heroes';
+    title = 'Tour of Heroes';
 }
